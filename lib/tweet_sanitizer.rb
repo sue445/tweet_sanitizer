42e31dfd47af50e430e9b42962086bff3eb8017d
sue445 <sue445@sue445.net>
2017-08-13 23:25:34 +0900

    Impl expand_urls_text

--- lib/tweet_sanitizer.rb
+++ lib/tweet_sanitizer.rb
@@ -1,5 +1,17 @@
 require "tweet_sanitizer/version"
+require "twitter"
 
 module TweetSanitizer
-  # Your code goes here...
+  # @param tweet [Twitter:Tweet]
+  # @return [String]
+  def self.expand_urls_text(tweet)
+    text = tweet.text.dup
+    return text unless tweet.uris?
+
+    tweet.uris.reverse.each_with_object(text) do |uri, expanded|
+      pos1 = uri.indices[0]
+      pos2 = uri.indices[1]
+      expanded[pos1, pos2] = uri.expanded_url
+    end
+  end
 end
--- spec/tweet_sanitizer_spec.rb
+++ spec/tweet_sanitizer_spec.rb
@@ -1,11 +1,35 @@
-require "spec_helper"
-
 RSpec.describe TweetSanitizer do
-  it "has a version number" do
-    expect(TweetSanitizer::VERSION).not_to be nil
-  end
+  describe ".expand_urls_text" do
+    subject { TweetSanitizer.expand_urls_text(tweet) }
 
-  it "does something useful" do
-    expect(false).to eq(true)
+    where do
+      {
+        "https://twitter.com/sue445/status/866636479061147648" => {
+          id: 866636479061147648,
+          text: "“GitHubのリポジトリをDprecatedにするスクリプト | Web Scratch” https://t.co/vG7cvDAMEb",
+          urls: [
+            {
+              url:          "https://t.co/vG7cvDAMEb",
+              expanded_url: "http://htn.to/RC5eJf",
+              display_url:  "htn.to/RC5eJf",
+              indices:      [47, 70],
+            }
+          ],
+          expected: "“GitHubのリポジトリをDprecatedにするスクリプト | Web Scratch” http://htn.to/RC5eJf",
+        },
+      }
+    end
+
+    with_them do
+      let(:tweet) do
+        Twitter::Tweet.new(
+          id:       id,
+          text:     text,
+          entities: { urls: urls }
+        )
+      end
+
+      it { should eq expected }
+    end
   end
 end

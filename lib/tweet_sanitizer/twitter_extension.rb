404d0338233ed8e8b037a50be90e8906ea197492
sue445 <sue445@sue445.net>
2022-10-05 19:38:51 +0900

    Fix YARD

--- lib/tweet_sanitizer/twitter_extension.rb
+++ lib/tweet_sanitizer/twitter_extension.rb
@@ -1,12 +1,17 @@
 module TweetSanitizer
   module TwitterExtension
+    # Sanitize tweet
+    #
+    # @!method sanitized_text(use_retweeted_tweet: true, expand_url: true, remove_media_url: true, unescape: true)
+    #
+    # @param use_retweeted_tweet [Boolean] Whether use original retweeted tweet if exists
+    # @param expand_url          [Boolean] Whether expand url in tweet (e.g. `t.co` url -> original url)
+    # @param remove_media_url    [Boolean] Whether remove media url in tweet
+    # @param unescape            [Boolean] Whether unescape in tweet (e.g. `(&gt; &lt;)` -> `(> <)`)
+    #
+    # @return [String]
+
     refine(Twitter::Tweet) do
-      # Sanitize tweet
-      # @param use_retweeted_tweet [Boolean] Whether use original retweeted tweet if exists
-      # @param expand_url          [Boolean] Whether expand url in tweet (e.g. `t.co` url -> original url)
-      # @param remove_media_url    [Boolean] Whether remove media url in tweet
-      # @param unescape            [Boolean] Whether unescape in tweet (e.g. `(&gt; &lt;)` -> `(> <)`)
-      # @return [String]
       def sanitized_text(use_retweeted_tweet: true, expand_url: true, remove_media_url: true, unescape: true)
         TweetSanitizer.sanitize(
           self,
